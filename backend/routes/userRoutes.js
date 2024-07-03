8370b099e6f01ab9cf1e50f4e1eb80fc2aa7245a
Poushik <poushikyelne750@gmail.com>
2024-07-03 22:48:15 +0530

    user authentication done backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,12 @@ import {
   getMe,
 } from "../controllers/userController.js";
 
-// import { protect } from "../middleware/authMiddleware.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-// router.get("/me", protect, getMe);
+router.get("/me", protect, getMe);
 
 export default router;
