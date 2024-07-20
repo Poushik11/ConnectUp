f487e9100df415224fb7b66083547ab0ec934add
Poushik <poushikyelne750@gmail.com>
2024-07-20 19:47:37 +0530

    Tickets add backend controllers model and routes added private route enabled

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    company: {
+      type: String,
+      required: false,
+      ref: "User",
+    },
+    task: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    product: {
+      type: String,
+      required: [true, "Please select a Ticket"],
+      enum: [
+        "Oncampus",
+        "OffCampus",
+        "PrePlacment-Talk",
+        "Task",
+        "Meeting",
+        "Presentation",
+        "Meet-Up",
+      ],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter a description of the issue"],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["new", "open", "closed"],
+      default: "new",
+    },
+    date: {
+      type: Date,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Ticket = mongoose.model("Ticket", ticketSchema);
+export default Ticket;
+
+// module.exports = mongoose.model("Ticket", ticketSchema);
--- fronted/src/App.js
+++ fronted/src/App.js
@@ -2,13 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
-// import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-
-// import NewTicket from "./pages/NewTicket";
+import NewTicket from "./pages/NewTickets";
 // import Tickets from "./pages/Tickets";
 // import Ticket from "./pages/Ticket";
 
@@ -25,14 +24,16 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
-            {/* <Route
-              path="/new-ticket"
+
+            <Route
+              path="/New-ticket"
               element={
                 <PrivateRoute>
                   <NewTicket />
                 </PrivateRoute>
               }
             />
+            {/*
             <Route
               path="/tickets"
               element={
